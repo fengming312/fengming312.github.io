061881680a082368e998c1264aa91b57a725dab2
nil <787190915@qq.com>
2016-09-08 00:54:50 +0800

    2

--- js/index.js
+++ js/index.js
@@ -17,12 +17,45 @@ function getScrollTop() {
 window.onscroll=function () {
     var scrollHeight=getScrollTop().scrollTop;
     console.log(scrollHeight);
-    if (scrollHeight<=100) {
+    if (scrollHeight<50) {
+        $(".fm-name").removeClass("bounceInRight animated")
+        $(".fm-info").removeClass("bounceInLeft animated")
+    }
+    if (scrollHeight>50&scrollHeight<=200) {
+        $(".fm-name").addClass("bounceInRight animated")
+        $(".fm-info").addClass("bounceInLeft animated")
         $(".fm-image>img").removeClass("hover")
     }
-    else if(scrollHeight>100&scrollHeight<=300) {
+    if (scrollHeight>200&scrollHeight<=300) {
         $(".fm-image>img").addClass("hover")
-    }else if (scrollHeight>300){
+        $(".fm-image>img").removeClass("flip animated")
+    }
+
+    if (scrollHeight>300&scrollHeight<=600){
         $(".fm-image>img").removeClass("hover")
+        $(".fm-image>img").addClass("flip animated")
+        $(".drop-shadow").removeClass("about-hover")
+        $(".about-grids").removeClass("grids-hover")
+    }
+    if (scrollHeight>600){
+        $(".drop-shadow").addClass("about-hover")
+    }
+    if (scrollHeight>800){
+        $(".drop-shadow").removeClass("about-hover")
+        $(".about-grids").addClass("grids-hover")
+    }
+    if (scrollHeight<1000){
+        $(".skills .dance").css({"opacity":'0.2','left':'0'})
+    }
+    if (scrollHeight>1000){
+        $(".skills .dance").css({"opacity":'1','left':'80%'})
+    }
+    if (scrollHeight>1300){
+        $(".skills .dance").addClass("flip animated")
+    }
+    if (scrollHeight>1600){
+        $(".skills .dance").removeClass("flip animated")
+        $(".skills .dance").addClass("hinge animated")
+
     }
 }
