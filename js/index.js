a5585bad88026e0777d7839f75ebe7d277b4c896
nil <787190915@qq.com>
2016-09-07 17:27:46 +0800

    2

--- js/index.js
+++ js/index.js
@@ -3,3 +3,26 @@ jQuery(".foucebox").slide({ mainCell:".bd ul", effect:"fold", autoPlay:true, del
     jQuery(".foucebox .hoverBg").animate({"margin-top":100*i},150);
 }
 });
+
+
+function getScrollTop() {
+    var scrollTop=window.pageYOffset||document.documentElement.scrollTop||document.body.scrollTop||0;
+    var scrollLeft=window.pageXOffset||document.documentElement.scrollLeft||document.body.scrollLeft||0;
+    return {
+        scrollTop:scrollTop,
+        scrollLeft:scrollLeft
+    }
+}
+
+window.onscroll=function () {
+    var scrollHeight=getScrollTop().scrollTop;
+    console.log(scrollHeight);
+    if (scrollHeight<=100) {
+        $(".fm-image>img").removeClass("hover")
+    }
+    else if(scrollHeight>100&scrollHeight<=300) {
+        $(".fm-image>img").addClass("hover")
+    }else if (scrollHeight>300){
+        $(".fm-image>img").removeClass("hover")
+    }
+}
